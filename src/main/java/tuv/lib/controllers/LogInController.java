faeb2824208d33976ba6023e40369d58928c7c73
Zheni Mincheva <zheni.mincheva@gmail.com>
2019-10-25 00:53:37 +0300

    Added login functionality.

--- src/main/java/tuv/lib/controllers/LogInController.java
+++ src/main/java/tuv/lib/controllers/LogInController.java
@@ -2,6 +2,8 @@ package tuv.lib.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -11,14 +13,23 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import tuv.lib.models.DBConnector;
 
 public class LogInController implements Initializable {
 
 	@FXML
 	private Button btn_logIn;
+
 	@FXML
 	private Button btn_cancel;
+
+	@FXML
+	private TextField userNameTextBox;
+
+	@FXML
+	private TextField passwordTextBox;
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
@@ -26,18 +37,41 @@ public class LogInController implements Initializable {
 	@FXML
 	private void login(ActionEvent event) {
 
-		System.out.println("some text");
+		// System.out.println("some text");
+
+		DBConnector.setUpConncetion();
+
+		String query = "SELECT USER_POSS\r\n" + "FROM libr.users\r\n" + "WHERE USER_NAME = \""
+				+ userNameTextBox.getText() + "\" AND USER_PASSWORD = \"" + passwordTextBox.getText() + "\"; ";
+
+		String address = "";
 
 		try {
+
+			ResultSet rs = DBConnector.executeQuery(query);
+
 			btn_logIn.getScene().getWindow().hide();
 			Stage admin = new Stage();
-			Parent root = FXMLLoader.load(getClass().getResource("../../../views/AdminPannel.fxml"));
+
+			if (rs.next()) {
+
+				if (rs.getInt(1) == 0) {
+					address = "../../../views/AdminPannel.fxml";
+				} else if (rs.getInt(1) == 1) {
+					address = "../../../views/OperatorPannel.fxml";
+				}
+			}
+
+			Parent root = FXMLLoader.load(getClass().getResource(address));
 			Scene scene = new Scene(root);
 			admin.setScene(scene);
 			admin.show();
 			admin.setResizable(false);
-		
+
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- src/main/java/tuv/lib/models/DBConnector.java
+++ src/main/java/tuv/lib/models/DBConnector.java
@@ -17,21 +17,35 @@ final public class DBConnector {
 	public static void setUpConncetion() {
 
 		String query = "SELECT VERSION()";
-
+		String query2 = "SELECT USER_POSS\r\n" + 
+				"FROM libr.users\r\n" + 
+				"WHERE USER_NAME = \"Clara\" AND USER_PASSWORD = \"pass3\"; ";
 		try {
 			con = DriverManager.getConnection(url, user, password);
 
 			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(query);
+			ResultSet rs = st.executeQuery(query2);
 
 			if (rs.next()) {
 
-				System.out.println(rs.getString(1));
+				System.out.println(rs.getInt(1));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public static ResultSet executeQuery(String query) {
+		ResultSet rs = null;
+		try {
+			Statement st = con.createStatement();
+			rs = st.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
 	}
 
 }
